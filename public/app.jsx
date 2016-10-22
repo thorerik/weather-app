5dcb11f529d6ec2b81b9ac8c4b58cb83f3204b5e
Thor Erik Lie <thor@thorerik.com>
2016-10-22 22:06:05 +0200

    mrow

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,43 @@
 var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: '💣',
+      message: '🐭🐈😸'
+    };
+  },
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function (e) {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+    this.refs.name.value = "";
+
+    this.setState({
+      name: name
+    });
+  },
   render: function () {
+    var name = this.state.name;
+    var message = this.props.message;
+
     return (
       <div>
-        <h1>Meow 🐈</h1>
-        <p>🐭🐈😸</p>
+        <h1>Meow 🐈 {name}</h1>
+        <p>{message}</p>
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set name</button>
+        </form>
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name="👯" message="🖕🏻"/>,
   document.getElementById('app')
 );
