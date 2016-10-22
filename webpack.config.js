e27e073b9e7ef996ad0ed9740ce4bb59bac7dc49
Thor Erik Lie <thor@thorerik.com>
2016-10-22 23:24:24 +0200

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './public/app.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: [
+      '',
+      '.js',
+      '.jsx'
+    ]
+  }
+};
