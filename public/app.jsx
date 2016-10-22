d9cb333e09f018609878c1aacff1447d0bf97e2c
Thor Erik Lie <thor@thorerik.com>
2016-10-22 23:33:09 +0200

    webpack working bitches

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom')
 var GreeterMessage = React.createClass({
   render: function () {
     var name = this.props.name;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './public/app.js',
+  entry: './public/app.jsx',
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -9,6 +9,21 @@ module.exports = {
       '',
       '.js',
       '.jsx'
+    ]
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            'react',
+            'es2015'
+          ]
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
     ]
   }
 };
