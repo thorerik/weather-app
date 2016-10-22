094cdb6fbfa49b9d37db7788438742877f31d5cc
Thor Erik Lie <thor@thorerik.com>
2016-10-22 23:59:46 +0200

    mk boilerplate

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Greeter = require('Greeter');
-
 ReactDOM.render(
-  <Greeter/>,
+  <h1>Boilerplate</h1>,
   document.getElementById('app')
 );
