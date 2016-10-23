3dce14f09f70f5ff93f1b6e1f77e12d556eff59c
Thor Erik Lie <thor@thorerik.com>
2016-10-23 15:17:07 +0200

    about bisch

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,10 @@ var React = require('react');
 var About = () => {
   return (
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
       <p>Tuxy is learning react and shit.</p>
+      <p>Days without merge conflict</p>
+      <div class="stat">∞</div>
     </div>
   );
 };
