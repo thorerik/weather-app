0199eb8b5b20757dc7eb2db2a855d5ba6727230b
Thor Erik Lie <thor@thorerik.com>
2016-10-23 14:06:15 +0200

    woof

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var express = require('express');
 
 // create our app
 var app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, function () {
-  console.log(`Server is up at :${port}`);
+app.listen(PORT, function () {
+  console.log(`Server is up at :${PORT}`);
 });
