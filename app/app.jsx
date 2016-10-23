fb923e7024518878292941fa866acbee7295a6f1
Thor Erik Lie <thor@thorerik.com>
2016-10-23 17:39:55 +0200

    scss!

--- app/app.jsx
+++ app/app.jsx
@@ -12,7 +12,7 @@ require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 // load our own custom styles
-require('style!css!applicationStyles');
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
   <Router history={hashHistory}>
